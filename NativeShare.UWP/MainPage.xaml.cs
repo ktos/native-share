2afbc4540b45bf6eb9241d8b54feb5b79d9d3b6a
Marcin Badurowicz <m@badurowicz.net>
2018-09-26 22:44:22 +0200

    Add basic UI for protocol activation

--- NativeShare.UWP/MainPage.xaml.cs
+++ NativeShare.UWP/MainPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,45 @@ namespace NativeShare.UWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private string data;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+        private void Dtm_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            args.Request.Data.Properties.Title = "Native Share URI";
+            args.Request.Data.SetWebLink(new Uri(data));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dtm = DataTransferManager.GetForCurrentView();
+            dtm.DataRequested += Dtm_DataRequested;
+            dtm.TargetApplicationChosen += Dtm_TargetApplicationChosen;
+
+            DataTransferManager.ShowShareUI(new ShareUIOptions() { Theme = ShareUITheme.Dark });
+        }
+
+        private void Dtm_TargetApplicationChosen(DataTransferManager sender, TargetApplicationChosenEventArgs args)
+        {
+            
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var args = e.Parameter as ProtocolActivatedEventArgs;
+            if (args != null)
+            {
+                var w = new WwwFormUrlDecoder(args.Uri.Query);
+
+                if (w.Count == 1 && w[0].Name == "value")
+                {
+                    data = w[0].Value;
+                    Button_Click(null, null);
+                }
+            }
         }
     }
 }
